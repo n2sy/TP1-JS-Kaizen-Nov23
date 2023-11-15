8ceb3cf6aacb5ba9f947d6ad58c92b7efd92624b
n2sy <nidhal.jelassi@fsegt.utm.tn>
2023-11-15 17:26:01 +0100

    seconde partie ex5

--- ex5.js
+++ ex5.js
@@ -1,24 +1,52 @@
-const guests= ['Madrid', 'Lisbonne', 'Paris', 'Lisbonne', 'Lisbonne',
-    'Paris', 'Lisbonne'];
+// const guests= ['Madrid', 'Lisbonne', 'Paris', 'Lisbonne', 'Lisbonne',
+//     'Paris', 'Lisbonne'];
 
-var stats = new Map();
+// var stats = new Map();
 
-guests.forEach((element) => {
-    console.log(element);
-    console.log(stats)
-    if(!stats.has(element)) {
-        stats.set(element, 1)
-    } else {
-        let nb = stats.get(element);
-        //nb++;
-        stats.set(element, ++nb); // increment Pre
-        //stats.set(element, nb++); // increment Post
+// guests.forEach((element) => {
+//     console.log(element);
+//     console.log(stats)
+//     if(!stats.has(element)) {
+//         stats.set(element, 1)
+//     } else {
+//         let nb = stats.get(element);
+//         //nb++;
+//         stats.set(element, ++nb); // increment Pre
+//         //stats.set(element, nb++); // increment Post
 
-    }
+//     }
+// })
+
+// console.log(stats);
+
+// for (const [key, value] of stats) {
+//     console.log(`${key} : ${value} visiteurs`);
+// }
+
+
+/////////////////////////////////////////////////////////////////////
+
+const users = [
+    {id: 1, revenus:[10, 30]},
+    {id: 2, revenus:[10, 40]},
+    {id: 3, revenus:[40, 40]}
+];
+
+newUsers = users.map((user) => {
+   const iter = user.revenus.values(); 
+    let somme = 0;
+   for (const val of iter) {
+        somme += val;
+   }
+   return {id : user.id, total : somme}
 })
 
-console.log(stats);
+console.log(newUsers);
 
-for (const [key, value] of stats) {
-    console.log(`${key} : ${value} visiteurs`);
-}
+// const tab = [2, 3, 5];
+
+// newTab = tab.map((elt) => {
+//     return elt * 5;
+// })
+
+// console.log(newTab);
